adf626edadaf8bb82df8a5219093f4cfe8f09733
Robert Arcaleanu <alumnerobert@gmail.com>
2023-05-03 21:21:48 +0200

    added new projects

---
+++ Project01__911-calls.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:/Users/rober/OneDrive/Escriptori/DataSets/Python/Refactored_Py_DS_ML_Bootcamp-master/10-Data"
+                 "-Capstone-Projects/911.csv")
+df.head()
