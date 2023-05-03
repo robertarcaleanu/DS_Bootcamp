6b1fbaa787604c6028b2e2a63a09ea5ef2b794cb
Robert Arcaleanu <alumnerobert@gmail.com>
2023-05-03 21:37:49 +0200

    [Project01] - Completed Questions section

--- Project01__911-calls.py
+++ Project01__911-calls.py
@@ -17,8 +17,11 @@ df['twp'].value_counts()
 # 3. Take a look at the 'title' column, how many unique title codes are there?
 df['title'].nunique()
 
-# Creating new features
-# 1. Obtain reason
-# 2. What is the most common Reason for a 911 call based off of this new column?
-# 3. Now use seaborn to create a countplot of 911 calls by Reason
-# 4. Now let us begin to focus on time information. What is the data type of the objects in the timeStamp column?
+# Creating new features 1. Obtain reason 2. What is the most common Reason for a 911 call based off of this new
+# column? 3. Now use seaborn to create a countplot of 911 calls by Reason 4. Now let us begin to focus on time
+# information. What is the data type of the objects in the timeStamp column?
+# 5. You should have seen that these
+# timestamps are still strings. Use [pd.to_datetime](
+# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.to_datetime.html) to convert the column from strings
+# to DateTime objects.
+# 6. Create 3 columns called Hour, Month and Day of the Week
